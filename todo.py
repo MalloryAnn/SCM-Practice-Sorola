e0ea860122b58ef50d9cdbfcc4cba41dba25faba
Mallory Sorola <127247712+MalloryAnn@users.noreply.github.com>
2025-03-25 13:08:56 -0500

    Update todo.py
    Bug fixed: prevented adding empty tasks to the list

--- todo.py
+++ todo.py
@@ -3,15 +3,20 @@
 tasks = []
 
 def add_task(task):
+    if task.strip() == "":
+        print("Error: Task cannot be empty.")
+        return
     tasks.append(task)
     print(f"Added task: {task}")
+    
 
 def view_tasks():
     print("Your Tasks:")
     for i, task in enumerate(tasks, start=1):
         print(f"{i}. {task}")
 
-# Demo
-add_task("Finish GitHub assignment")
+
+
+add_task("Finish Software Engin GitHub assignment")
 add_task("Buy groceries")
 view_tasks()
