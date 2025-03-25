a1b136dc831a631d4110ee008019913dd84ffb2c
Mallory Sorola <127247712+MalloryAnn@users.noreply.github.com>
2025-03-25 13:21:01 -0500

    add delete feature
    feature: added delete function to remove tasks by number

--- todo.py
+++ todo.py
@@ -8,15 +8,21 @@ def add_task(task):
         return
     tasks.append(task)
     print(f"Added task: {task}")
-    
 
 def view_tasks():
     print("Your Tasks:")
     for i, task in enumerate(tasks, start=1):
         print(f"{i}. {task}")
 
-
+def delete_task(task_number):
+    if 1 <= task_number <= len(tasks):
+        removed = tasks.pop(task_number - 1)
+        print(f"Deleted task: {removed}")
+    else:
+        print("Error: Invalid task number.")
 
 add_task("Finish Software Engin GitHub assignment")
 add_task("Buy groceries")
+view_tasks()
+delete_task(1)
 view_tasks()
